9c5ee10e9774580290bfc8558616891a90142149
Nickolas <lorddarthveider7@yandex.ru>
2016-09-10 11:25:06 +0400

    Find dialog was redesigned

--- dialogfind.py
+++ dialogfind.py
@@ -1,4 +1,5 @@
 import tkinter
+import tkinter.ttk
 from base_classes import *
 
 
@@ -8,8 +9,10 @@ class DialogFind:
 
         self.window = tkinter.Toplevel(master)
         self.window.geometry("320x320+500+100")
+        self.window.resizable(height=False, width=False)
         self.main_frame = MyFrame(self.window)
         self.main_frame.pack(fill='both', expand='yes')
+
 
         self.text_search_frame = MyFrame(self.main_frame)
         self.text_search_frame.pack(side='top', expand='no', fill='x')
@@ -17,13 +20,6 @@ class DialogFind:
         self.text_search_label.pack(side='left', expand='no')
         self.text_search_entry = tkinter.Entry(self.text_search_frame)
         self.text_search_entry.pack(side='right', expand='yes', fill='x')
-
-        self.tag_frame = MyFrame(self.main_frame)
-        self.tag_frame.pack(side='top', expand='no', fill='x')
-        self.tag_label = tkinter.Label(self.tag_frame, text='#tag:')
-        self.tag_label.pack(side='left', expand='no')
-        self.tag_entry = tkinter.Entry(self.tag_frame)
-        self.tag_entry.pack(side='right', expand='yes', fill='x')
 
         self.button_frame = MyFrame(self.main_frame)
         self.button_frame.pack(side='bottom', fill='x')
@@ -36,10 +32,11 @@ class DialogFind:
         self.radio_button_frame = MyFrame(self.main_frame)
         self.radio_button_frame.pack(side='bottom', fill='both')
 
+        """
         self.radio_var = tkinter.IntVar()
         self.radio_button_list = []
         self.radio_frame_list = []
-        for (index, button) in enumerate(("Search by text", "Search by tag", "Search by date")):
+        for (index, button) in enumerate(("Search by text", "Search by tag")):
             self.radio_frame_list.append(MyFrame(self.radio_button_frame))
             self.radio_frame_list[-1].pack(fill='both')
             self.radio_button_list.append(tkinter.Radiobutton(self.radio_frame_list[-1],
@@ -47,22 +44,44 @@ class DialogFind:
                                                               variable=self.radio_var,
                                                               value=index))
             self.radio_button_list[-1].pack(side='left', fill='x')
-
+        """
         self.window.protocol("WM_DELETE_WINDOW", self.cancel_command)
 
+        self.search_type = tkinter.StringVar()
+        self.is_text_search_radiobutton = tkinter.Radiobutton(self.radio_button_frame,
+                                                              text="Search by text",
+                                                              variable=self.search_type,
+                                                              value="text")
+        self.is_text_search_radiobutton.pack(anchor="nw", padx=15)
+        self.is_text_search_radiobutton.select()
+
+        self.is_tag_search_radiobutton = tkinter.Radiobutton(self.radio_button_frame,
+                                                             text="Search by tag",
+                                                             variable=self.search_type,
+                                                             value="tag")
+        self.is_tag_search_radiobutton.pack(anchor='nw', padx=15)
+        self.is_tag_search_radiobutton.deselect()
+
+        self.is_date_used = tkinter.IntVar()
+        self.is_date_used_checkbox = tkinter.Checkbutton(self.radio_button_frame,
+                                                         text='Search by data',
+                                                         variable=self.is_date_used,
+                                                         command=self.unable_timeinterval)
+        self.is_date_used_checkbox.pack(anchor='nw', padx=15)
+
+        self.is_timeinterval_used = tkinter.IntVar()
+        self.is_timeinterval_used_checkbox = tkinter.Checkbutton(self.radio_button_frame,
+                                                                 text='Search in timeinterval',
+                                                                 variable=self.is_timeinterval_used,
+                                                                 state='disabled',
+                                                                 command=self.unable_time)
+        self.is_timeinterval_used_checkbox.pack(anchor='nw', padx=35)
         # TODO: add search by data
 
         self.date_frame = MyFrame(self.main_frame)
-        self.date_frame.pack(side='top', expand='no', fill='both')
+        self.date_frame.pack(side='top', expand='yes', fill='both')
 
-        self.date_label = tkinter.Label(self.date_frame, text="Date:")
-        self.date_label.pack(side='left', expand='no')
-        self.date_day = tkinter.ttk.Combobox(self.date_frame, width=3, values=[x for x in range(1, 32)])
-        self.date_day.pack(side='left', expand='no')
-        self.date_month = tkinter.ttk.Combobox(self.date_frame, width=3, values=[x for x in range(1, 13)])
-        self.date_month.pack(side='left', expand='no')
-        self.date_year = tkinter.ttk.Combobox(self.date_frame, width=5, values=[x for x in range(2016, 2030)])
-        self.date_year.pack(side='left', expand='no')
+        self.create_date_widgets(self.date_frame)
 
     def go(self):
         self.window.grab_set()
@@ -91,3 +110,84 @@ class DialogFind:
     def cancel_command(self):
         self.return_value = None
         self.window.destroy()
+
+    def unable_timeinterval(self):
+        if self.is_timeinterval_used_checkbox['state'] == 'disabled':
+            self.is_timeinterval_used_checkbox['state'] = 'normal'
+        else:
+            self.is_timeinterval_used_checkbox['state'] = 'disabled'
+            self.is_timeinterval_used_checkbox.deselect()
+        self.unable_time()
+
+    def create_date_widgets(self, date_frame):
+        self.date_label = tkinter.Label(date_frame, text="Date:")
+        self.date_label.pack(anchor='nw', expand='no')
+
+        self.date_start_frame = MyFrame(date_frame)
+        self.date_start_frame.pack(side='top', fill='x')
+        self.date_start_label = tkinter.Label(self.date_start_frame, text="Begin:", state='disabled')
+        self.date_start_label.pack(side='left', expand='no', padx=20)
+        self.date_start_day = tkinter.ttk.Combobox(self.date_start_frame,
+                                                   width=3,
+                                                   values=[x for x in range(1, 32)],
+                                                   state='disabled')
+        self.date_start_day.pack(side='left', expand='no')
+        self.date_start_month = tkinter.ttk.Combobox(self.date_start_frame,
+                                                     width=3,
+                                                     values=[x for x in range(1, 13)],
+                                                     state='disabled')
+        self.date_start_month.pack(side='left', expand='no')
+        self.date_start_year = tkinter.ttk.Combobox(self.date_start_frame,
+                                                    width=5,
+                                                    values=[x for x in range(2016, 2030)],
+                                                    state='disabled')
+        self.date_start_year.pack(side='left', expand='no')
+
+        self.date_end_frame = MyFrame(date_frame)
+        self.date_end_frame.pack(side='top', fill='x')
+        self.date_end_label = tkinter.Label(self.date_end_frame, text="End:", state='disabled')
+        self.date_end_label.pack(side='left', expand='no', padx=25)
+        self.date_end_day = tkinter.ttk.Combobox(self.date_end_frame,
+                                                 width=3,
+                                                 values=[x for x in range(1, 32)],
+                                                 state='disabled')
+        self.date_end_day.pack(side='left', expand='no')
+        self.date_end_month = tkinter.ttk.Combobox(self.date_end_frame,
+                                                   width=3,
+                                                   values=[x for x in range(1, 13)],
+                                                   state='disabled')
+        self.date_end_month.pack(side='left', expand='no')
+        self.date_end_year = tkinter.ttk.Combobox(self.date_end_frame,
+                                                  width=5,
+                                                  values=[x for x in range(2016, 2030)],
+                                                  state='disabled')
+        self.date_end_year.pack(side='left', expand='no')
+
+    def unable_time(self):
+        if self.is_date_used.get():
+            self.date_start_label['state'] = 'normal'
+            self.date_start_day['state'] = 'normal'
+            self.date_start_month['state'] = 'normal'
+            self.date_start_year['state'] = 'normal'
+        else:
+            self.date_start_label['state'] = 'disabled'
+            self.date_start_day['state'] = 'disabled'
+            self.date_start_month['state'] = 'disabled'
+            self.date_start_year['state'] = 'disabled'
+
+        if self.is_timeinterval_used.get():
+            self.date_end_label['state'] = 'normal'
+            self.date_end_day['state'] = 'normal'
+            self.date_end_month['state'] = 'normal'
+            self.date_end_year['state'] = 'normal'
+        else:
+            self.date_end_label['state'] = 'disabled'
+            self.date_end_day['state'] = 'disabled'
+            self.date_end_month['state'] = 'disabled'
+            self.date_end_year['state'] = 'disabled'
+
+
+if __name__ == '__main__':
+    root = tkinter.Tk()
+    dialog = DialogFind(root)
+    print(dialog.go())
